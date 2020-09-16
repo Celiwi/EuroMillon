c37767256da147a5eac6d5de17efd8cc157f4f81
Celiwi <58071688+Celiwi@users.noreply.github.com>
2020-09-16 18:49:21 +0200

    version 1

---
+++ EuroMillon/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace EuroMillon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textNum1 = new System.Windows.Forms.TextBox();
+            this.textNum2 = new System.Windows.Forms.TextBox();
+            this.textNum3 = new System.Windows.Forms.TextBox();
+            this.textNum4 = new System.Windows.Forms.TextBox();
+            this.textNum5 = new System.Windows.Forms.TextBox();
+            this.textNum6 = new System.Windows.Forms.TextBox();
+            this.columna1 = new System.Windows.Forms.Panel();
+            this.columna2 = new System.Windows.Forms.Panel();
+            this.n6 = new System.Windows.Forms.ListBox();
+            this.n5 = new System.Windows.Forms.ListBox();
+            this.n4 = new System.Windows.Forms.ListBox();
+            this.n3 = new System.Windows.Forms.ListBox();
+            this.n2 = new System.Windows.Forms.ListBox();
+            this.n1 = new System.Windows.Forms.ListBox();
+            this.btnComprobar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textAciertos = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.columna1.SuspendLayout();
+            this.columna2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textNum1
+            // 
+            this.textNum1.Location = new System.Drawing.Point(49, 26);
+            this.textNum1.Name = "textNum1";
+            this.textNum1.Size = new System.Drawing.Size(100, 22);
+            this.textNum1.TabIndex = 0;
+            // 
+            // textNum2
+            // 
+            this.textNum2.Location = new System.Drawing.Point(49, 94);
+            this.textNum2.Name = "textNum2";
+            this.textNum2.Size = new System.Drawing.Size(100, 22);
+            this.textNum2.TabIndex = 1;
+            // 
+            // textNum3
+            // 
+            this.textNum3.Location = new System.Drawing.Point(49, 163);
+            this.textNum3.Name = "textNum3";
+            this.textNum3.Size = new System.Drawing.Size(100, 22);
+            this.textNum3.TabIndex = 2;
+            // 
+            // textNum4
+            // 
+            this.textNum4.Location = new System.Drawing.Point(49, 226);
+            this.textNum4.Name = "textNum4";
+            this.textNum4.Size = new System.Drawing.Size(100, 22);
+            this.textNum4.TabIndex = 3;
+            // 
+            // textNum5
+            // 
+            this.textNum5.Location = new System.Drawing.Point(49, 290);
+            this.textNum5.Name = "textNum5";
+            this.textNum5.Size = new System.Drawing.Size(100, 22);
+            this.textNum5.TabIndex = 4;
+            // 
+            // textNum6
+            // 
+            this.textNum6.Location = new System.Drawing.Point(49, 356);
+            this.textNum6.Name = "textNum6";
+            this.textNum6.Size = new System.Drawing.Size(100, 22);
+            this.textNum6.TabIndex = 5;
+            // 
+            // columna1
+            // 
+            this.columna1.Controls.Add(this.textNum1);
+            this.columna1.Controls.Add(this.textNum6);
+            this.columna1.Controls.Add(this.textNum2);
+            this.columna1.Controls.Add(this.textNum5);
+            this.columna1.Controls.Add(this.textNum3);
+            this.columna1.Controls.Add(this.textNum4);
+            this.columna1.Location = new System.Drawing.Point(48, 48);
+            this.columna1.Name = "columna1";
+            this.columna1.Size = new System.Drawing.Size(200, 390);
+            this.columna1.TabIndex = 6;
+            // 
+            // columna2
+            // 
+            this.columna2.Controls.Add(this.n6);
+            this.columna2.Controls.Add(this.n5);
+            this.columna2.Controls.Add(this.n4);
+            this.columna2.Controls.Add(this.n3);
+            this.columna2.Controls.Add(this.n2);
+            this.columna2.Controls.Add(this.n1);
+            this.columna2.Location = new System.Drawing.Point(594, 48);
+            this.columna2.Name = "columna2";
+            this.columna2.Size = new System.Drawing.Size(200, 390);
+            this.columna2.TabIndex = 7;
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.n6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.n6.FormattingEnabled = true;
+            this.n6.ItemHeight = 16;
+            this.n6.Location = new System.Drawing.Point(66, 346);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(58, 32);
+            this.n6.TabIndex = 5;
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.n5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.n5.FormattingEnabled = true;
+            this.n5.ItemHeight = 16;
+            this.n5.Location = new System.Drawing.Point(66, 280);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(58, 32);
+            this.n5.TabIndex = 4;
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.n4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.n4.FormattingEnabled = true;
+            this.n4.ItemHeight = 16;
+            this.n4.Location = new System.Drawing.Point(66, 216);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(78, 32);
+            this.n4.TabIndex = 3;
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.n3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.n3.FormattingEnabled = true;
+            this.n3.ItemHeight = 16;
+            this.n3.Location = new System.Drawing.Point(66, 153);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(58, 32);
+            this.n3.TabIndex = 2;
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.n2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.n2.FormattingEnabled = true;
+            this.n2.ItemHeight = 16;
+            this.n2.Location = new System.Drawing.Point(66, 84);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(58, 32);
+            this.n2.TabIndex = 1;
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.n1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.n1.FormattingEnabled = true;
+            this.n1.ItemHeight = 16;
+            this.n1.Location = new System.Drawing.Point(66, 26);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(58, 32);
+            this.n1.TabIndex = 0;
+            // 
+            // btnComprobar
+            // 
+            this.btnComprobar.BackColor = System.Drawing.Color.Linen;
+            this.btnComprobar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnComprobar.Location = new System.Drawing.Point(385, 113);
+            this.btnComprobar.Name = "btnComprobar";
+            this.btnComprobar.Size = new System.Drawing.Size(105, 39);
+            this.btnComprobar.TabIndex = 8;
+            this.btnComprobar.Text = "Comprobar";
+            this.btnComprobar.UseVisualStyleBackColor = false;
+            this.btnComprobar.Click += new System.EventHandler(this.btnComprobar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Linen;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.Location = new System.Drawing.Point(402, 358);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(105, 36);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 22);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Mis números";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(645, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 22);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ganadores";
+            // 
+            // textAciertos
+            // 
+            this.textAciertos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textAciertos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textAciertos.FormattingEnabled = true;
+            this.textAciertos.ItemHeight = 16;
+            this.textAciertos.Location = new System.Drawing.Point(419, 250);
+            this.textAciertos.Name = "textAciertos";
+            this.textAciertos.Size = new System.Drawing.Size(71, 34);
+            this.textAciertos.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Has acertado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(836, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textAciertos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnComprobar);
+            this.Controls.Add(this.columna2);
+            this.Controls.Add(this.columna1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.columna1.ResumeLayout(false);
+            this.columna1.PerformLayout();
+            this.columna2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textNum1;
+        private System.Windows.Forms.TextBox textNum2;
+        private System.Windows.Forms.TextBox textNum3;
+        private System.Windows.Forms.TextBox textNum4;
+        private System.Windows.Forms.TextBox textNum5;
+        private System.Windows.Forms.TextBox textNum6;
+        private System.Windows.Forms.Panel columna1;
+        private System.Windows.Forms.Panel columna2;
+        private System.Windows.Forms.ListBox n6;
+        private System.Windows.Forms.ListBox n5;
+        private System.Windows.Forms.ListBox n4;
+        private System.Windows.Forms.ListBox n3;
+        private System.Windows.Forms.ListBox n2;
+        private System.Windows.Forms.ListBox n1;
+        private System.Windows.Forms.Button btnComprobar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox textAciertos;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EuroMillon/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EuroMillon
+{
+    public partial class Form1 : Form
+    {
+        Random aleatorio = new Random();
+        int[] misNumeros = new int[6];
+        int[] numGanadores = new int[6];
+        //int[] numAcertados = new int[6];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComprobar_Click(object sender, EventArgs e)
+        {
+            //Variables
+            int aciertos = 0;
+
+            //Metemos los valores CORRECTOS de los textBox en un array
+            foreach (Control ctrl in columna1.Controls)
+            {
+                if(Convert.ToInt32(ctrl.Text) > 50 || Convert.ToInt32(ctrl.Text) < 1)
+                {
+                    MessageBox.Show("Números no válidos");
+                }
+                else
+                {
+                    for (int i = 0; i < misNumeros.Length; i++)
+                    {
+                        misNumeros[i] = Convert.ToInt32(ctrl.Text);
+                    }
+                }
+            }
+
+            //Llenamos un array con numers aleatorios hasta el 50 sin repeticiones
+            for(int i = 0; i < numGanadores.Length; i++)
+            {
+                Boolean continuar = true;
+
+                while (continuar)
+                {
+                    int num = aleatorio.Next(50);
+                    Boolean esta = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (numGanadores[j] == num)
+                        {
+                            esta = true;
+                        }
+                    }
+                    if (esta == false)
+                        continuar = false;
+                    numGanadores[i] = num;
+                }
+            }
+
+            //Mostramos los valores aleatorios
+            n1.Items.Add(numGanadores[0]);
+            n2.Items.Add(numGanadores[1]);
+            n3.Items.Add(numGanadores[2]);
+            n4.Items.Add(numGanadores[3]);
+            n5.Items.Add(numGanadores[4]);
+            n6.Items.Add(numGanadores[5]);
+
+            //Comparamos los array para ver si hay números premiados
+            for (int i = 0; i < misNumeros.Length; i++){
+                for(int j = 0; j < numGanadores.Length; j++)
+                {
+                    if(misNumeros[i] == numGanadores[j])
+                    {
+                        aciertos++;
+                        //numAcertados[i] = misNumeros[i];
+                    }
+                }
+            }
+
+            textAciertos.Items.Add(aciertos);
+
+        }
+
+        
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
